806298014fa8002e748c2d8979fe302c2e5b880b
gon <y2gon2@gmail.com>
2023-10-29 10:17:42 +0900

    #2 - Batch 설정 class 추가
    BatchConfig
     - 추후 Batch 설정을 구현할  config class 생성
    
    PassBatchApplication
     - BatchConfig 파일을 생성하여 분리 하였으므로 기존 applicaton 실행 class 의 @EnableBatchProcessing annotation 은 제거함.

--- src/main/java/com/fastcampus/pass/PassBatchApplication.java
+++ src/main/java/com/fastcampus/pass/PassBatchApplication.java
@@ -15,7 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 
-@EnableBatchProcessing
+
 @SpringBootApplication
 public class PassBatchApplication {
 

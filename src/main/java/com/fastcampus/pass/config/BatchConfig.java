427d916b6d876860d6859f1c0bba7d011fd7372c
gon <y2gon2@gmail.com>
2023-11-06 11:50:12 +0900

    #24 REST API 를 통한 Job 실행 요청 설정 구현
    JobLauncherRequest
     - REST API 로 어떤 값들을 받아 job 을 실행 시킬 것인지에 대한 요소를 설정 필요.  해당 설정에서는 JobName 과 JobParameter 를 받아서 실행에 필요한 최소  요소만으로 설정.
    
    JobLauncherController
     - URL 로 Job 실행을 요청할 것이므로,  web page 요청 때와 같이 controller 를 구현한다.
     - 실행될 job name 을 받아 jobRegistry 로 실행할 job 을 지정
     - job 실행에 필요한 job parameter 들을 받아 jobLauncher  실행 시 해당 값들을 넘겨 줌
    
    BatchConfig
     - REST  API job 실행 조건이 활성화하기 위해 configuration 에 관련 bean method 를 구현

--- src/main/java/com/fastcampus/pass/config/BatchConfig.java
+++ src/main/java/com/fastcampus/pass/config/BatchConfig.java
@@ -1,9 +1,24 @@
 package com.fastcampus.pass.config;
 
+import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @EnableBatchProcessing
 @Configuration
 public class BatchConfig {
+
+    /**
+     * JobRegistry 는 context 에서 Job 을 추적할 때 유용
+     * JobRegistryBeanPostProcessor 는 Application Context 가 올라오면서 Bean 등록 시,
+     * 자동으로 JobRegistry 에 Job 을 등록시켜 주어야  JobRegistry 에서 뽑아 쓸 수 있음.
+     * */
+    @Bean
+    public JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor(JobRegistry jobRegistry) {
+        JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor = new JobRegistryBeanPostProcessor();
+        jobRegistryBeanPostProcessor.setJobRegistry(jobRegistry);
+        return jobRegistryBeanPostProcessor;
+    }
 }
